08f73307084fe6439d87579ebac9941f669bd3cd
루리엘 <126620644+Luriel0228@users.noreply.github.com>
2023-09-18 10:27:23 +0900

    Update time.js

--- public/time.js
+++ public/time.js
@@ -4,20 +4,20 @@ function updateTime() {
     var now = new Date();
 
     document.getElementById("time").innerText =
-            zeroPadding(now.getHours(), 2) + ":" +
-            zeroPadding(now.getMinutes(), 2) + ":" +
-            zeroPadding(now.getSeconds(), 2);
+            timeindex(now.getHours(), 2) + ":" +
+            timeindex(now.getMinutes(), 2) + ":" +
+            timeindex(now.getSeconds(), 2);
 
     document.getElementById("date").innerText =
             now.getFullYear() + "-" +
-            zeroPadding(now.getMonth() + 1, 2) + "-" +
-            zeroPadding(now.getDate(), 2) + " " +
+            timeindex(now.getMonth() + 1, 2) + "-" +
+            timeindex(now.getDate(), 2) + " " +
             WEEK[now.getDay()] + "요일";
 }
 
 updateTime();
 setInterval(updateTime, 1000);
 
-function zeroPadding(num, digit) {
+function timeindex(num, digit) {
     return String(num).padStart(digit, '0');
 }
